a185890b1f2e219b0f6f2a4f75ea6843c7a8aaae
Yaekirua <111132084+Yaekirua@users.noreply.github.com>
2024-04-13 20:40:21 +0800

    Update script.js
    Nothing

--- script.js
+++ script.js
@@ -70,12 +70,54 @@ drone.on('error', error => {
 });
 
 function getRandomName() {
-  const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+  const char1 = [
+    "Lelouch",
+    "Light",
+    "Naruto",
+    "Ichigo",
+    "Monkey",
+    "Natsu",
+    "Edward",
+    "Erza",
+    "Roronoa",
+    "Luffy",
+    "Rukia",
+    "Gintoki",
+    "Alphonse",
+    "Kagome",
+    "Sousuke",
+    "Gaara",
+    "Hinata",
+    "Itachi",
+    "Orihime",
+    "Ken"
+  ];
+  const char2 = [
+    "Eren",
+    "Levi",
+    "Mikasa",
+    "Goku",
+    "Vegeta",
+    "Naruto",
+    "Sasuke",
+    "Tanjiro",
+    "Zenitsu",
+    "Inosuke",
+    "Deku",
+    "Bakugo",
+    "Todoroki",
+    "Luffy",
+    "Zoro",
+    "Sanji",
+    "Gon",
+    "Killua",
+    "Saitama",
+    "Genos"
+  ];
   return (
-    adjs[Math.floor(Math.random() * adjs.length)] +
+    char1[Math.floor(Math.random() * char1.length)] +
     "_" +
-    nouns[Math.floor(Math.random() * nouns.length)]
+    char2[Math.floor(Math.random() * char2.length)]
   );
 }
 
